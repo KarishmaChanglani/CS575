b9256ae8332dec0d381146dab2d57c2ad44e3d9b
Aditya Patwa <Aditya Patwa>
2016-12-08 14:08:23 -0500

    Formatting changes only.

--- backend/static/website/app/components/dashboard/DashboardModule.js
+++ backend/static/website/app/components/dashboard/DashboardModule.js
@@ -2,7 +2,12 @@ var dashboardModule = angular.module('DashboardModule', ['counter', 'chart.js'])
 
 dashboardModule.controller('DashboardController', function ($rootScope, $scope, $http, AppService) {
 
-    var parameters = {user: $rootScope.globals.currentUser.userId, category:'ip', start:0, count:10};
+    var parameters = {
+        user: $rootScope.globals.currentUser.userId,
+        category:'ip',
+        start:0,
+        count:10
+    };
     AppService.Users(parameters).
         then(function (response) {
             $scope.records = response.data.records;
