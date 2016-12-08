ba3e03dc73c5a021fb7d9103d1e6c4b0a5d6f1fa
Aditya Patwa <Aditya Patwa>
2016-12-08 13:36:00 -0500

    Added the module and view related to the machine component.

--- backend/static/website/app/components/machine/MachineModule.js
+++ backend/static/website/app/components/machine/MachineModule.js
@@ -1,6 +1,18 @@
 'use strict';
 
-var userModule = angular.module('UserModule', []);
+var machineModule = angular.module('MachineModule', []);
 
-userModule.controller('UserController', function ($scope) {
+machineModule.controller('MachineController', function ($rootScope, $scope, $routeParams, AppService) {
+    $scope.machineId = $routeParams.machineId;
+    var parameters = {
+        machine: $scope.machineId,
+        user: $rootScope.globals.currentUser.userId,
+        start:0,
+        count:100,
+        category: 'ip'
+    };
+    AppService.Machines(parameters).
+    then(function (response) {
+        $scope.records = response.data.records;
+    }, function (error) {});
 });
